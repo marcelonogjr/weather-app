48a3ae6c4f2a41c3c63ec7b0ca0360f701bbc545
marcelonogjr <97480299+marcelonogjr@users.noreply.github.com>
2022-05-20 23:15:55 -0300

    chage in token variable

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,11 +1,10 @@
 const axios = require('axios').default;
-// const tokens = require('./tokens');
+// const {geocodeToken} = require('./tokens');
 
 const geocode = async (address) => {
-  // const apiToken = tokens.geocodeToken;
-  const apiToken = process.env.GEOCODE_TOKEN;
+  const geocodeToken = process.env.GEOCODE_TOKEN;
 
-  const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${apiToken}&limit=1`;
+  const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${geocodeToken}&limit=1`;
 
   const response = await axios.get(mapBoxUrl).catch(() => {
     error: 'An error has occurred!\nLocation not found.';
