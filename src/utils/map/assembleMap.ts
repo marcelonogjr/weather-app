b9f0764bf265a6270d5fd09e5acd90021ac16fb0
marcelonogjr <97480299+marcelonogjr@users.noreply.github.com>
2022-05-25 23:41:35 -0300

    inverted the colors of the clouds layer for better UX

--- src/utils/map/assembleMap.ts
+++ src/utils/map/assembleMap.ts
@@ -11,6 +11,8 @@ const assembleMap = async (lat: number, lon: number, zoom: number, mapType: stri
   const width = 512;
   const canvas = createCanvas(height, width);
   const context = canvas.getContext('2d');
+  const canvas2 = createCanvas(height, width);
+  const context2 = canvas2.getContext('2d');
   
   const [tilesCoordinates, offsetCoordinates] = calculateCoordinates(lat, lon, zoom);
   const xLow = tilesCoordinates[0][0] < tilesCoordinates[3][0] ? tilesCoordinates[0][0] : tilesCoordinates[3][0];
@@ -23,8 +25,6 @@ const assembleMap = async (lat: number, lon: number, zoom: number, mapType: stri
   const xOffset = tileOffset[0] - offsetCoordinates[0];
   const yOffset = tileOffset[1] - offsetCoordinates[1];
 
-  // console.log(xOffset, yOffset);
-
   const mainImage = await loadImage(`https://api.mapbox.com/styles/v1/mapbox/light-v10/static/${lon},${lat},${zoom}/512x512?access_token=${geocodeToken}`);
   const topLeftImage = await loadImage(`https://tile.openweathermap.org/map/${mapType}/${zoom}/${xLow}/${yLow}.png?appid=${openWeatherToken}`);
   const topRightImage = await loadImage(`https://tile.openweathermap.org/map/${mapType}/${zoom}/${xHigh}/${yLow}.png?appid=${openWeatherToken}`);
@@ -32,11 +32,23 @@ const assembleMap = async (lat: number, lon: number, zoom: number, mapType: stri
   const bottomRightImage = await loadImage(`https://tile.openweathermap.org/map/${mapType}/${zoom}/${xHigh}/${yHigh}.png?appid=${openWeatherToken}`);
 
   context.drawImage(mainImage,0, 0, width, height);
-  // context.globalAlpha = 0.8;
-  context.drawImage(topLeftImage, -width/2 + xOffset, -height/2 + yOffset, width, height);
-  context.drawImage(topRightImage,width/2 + xOffset, -height/2 + yOffset, width, height);
-  context.drawImage(bottomLeftImage, -width/2 + xOffset, height/2 + yOffset, width, height);
-  context.drawImage(bottomRightImage,width/2 + xOffset, height/2 + yOffset, width, height);
+  context2.drawImage(topLeftImage, -width/2 + xOffset, -height/2 + yOffset, width, height);
+  context2.drawImage(topRightImage,width/2 + xOffset, -height/2 + yOffset, width, height);
+  context2.drawImage(bottomLeftImage, -width/2 + xOffset, height/2 + yOffset, width, height);
+  context2.drawImage(bottomRightImage,width/2 + xOffset, height/2 + yOffset, width, height);
+  
+  const imageData = context2.getImageData(0, 0, width, height);
+  
+  if (mapType === 'clouds_new'){
+    for (let i=0; i < imageData.data.length; i +=4) {
+      imageData.data[i] = 255 - imageData.data[i];
+      imageData.data[i+1] = 255 - imageData.data[i+1];
+      imageData.data[i+2] = 255 - imageData.data[i+2];
+    }
+  }
+
+  context2.putImageData(imageData, 0, 0);
+  context.drawImage(canvas2, 0, 0);
   
   const buffer = canvas.toBuffer('image/png');
   return buffer;
