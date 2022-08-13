cd1920f47fa0724a5f0c6dfae1216de9fa0a71ea
marcelonogjr <97480299+marcelonogjr@users.noreply.github.com>
2022-08-13 18:04:19 -0300

    simplified the logic to calculate the final weather layer offset

--- src/utils/map/calculateCoordinates.ts
+++ src/utils/map/calculateCoordinates.ts
@@ -21,17 +21,13 @@ const calculateCoordinates: CalculateCoordinatesType = (lat, lon, zoom) => {
     [xModified, yModified],
   ];
 
-  const latRad = (lat: number) => lat * Math.PI / 180;
-  const lonRad = (lon: number) => lon * Math.PI / 180;
+  const degreeToRad = (lat: number) => lat * Math.PI / 180;
   
-  const xPixels = (lon: number) => { return (512 / (2*Math.PI) * 2 ** zoom * (lon + Math.PI))};
-  const yPixels = (lat: number) => { return (512 / (2*Math.PI) * 2 ** zoom * (Math.PI - Math.log(Math.tan(Math.PI / 4 + lat / 2))))};
+  const xPixels = (lon: number) => { return ((512 / (2*Math.PI) * 2 ** zoom * (lon + Math.PI)) % 512)};
+  const yPixels = (lat: number) => { return ((512 / (2*Math.PI) * 2 ** zoom * (Math.PI - Math.log(Math.tan(Math.PI / 4 + lat / 2)))) % 512)};
 
-  const lonTile = xReal/Math.pow(2,zoom)*360-180;
-  const latTile = 180/Math.PI*Math.atan(0.5*(Math.exp(Math.PI-2*Math.PI*yReal/Math.pow(2,zoom))-Math.exp(-(Math.PI-2*Math.PI*yReal/Math.pow(2,zoom)))));
-
-  const offset = [xPixels(lonRad(lon)) - xPixels(lonRad(lonTile)), yPixels(latRad(lat)) - yPixels(latRad(latTile))];
-
+  const offset = [xPixels(degreeToRad(lon)), yPixels(degreeToRad(lat))];
+  
   return [tilesCoordinates, offset];
 };
 
