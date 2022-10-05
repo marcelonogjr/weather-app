13908686678ee9fe82a40cbf82076f0cdef77b08
marcelonogjr <97480299+marcelonogjr@users.noreply.github.com>
2022-10-05 01:07:11 -0300

    fixed react html paths to server API

--- react-app/src/components/Weather/WeatherInfo.tsx
+++ react-app/src/components/Weather/WeatherInfo.tsx
@@ -34,8 +34,8 @@ const WeatherInfo = () => {
   } = useContext(WeatherContext);
   const { zoom, mapLayer } = useContext(MapContext);
 
-  const serverUrl = 'http://localhost:5001';
-  // const serverUrl = 'https://weather.marcelojr.tech';
+  // const serverUrl = 'http://localhost:5001';
+  const serverUrl = 'https://weather.marcelojr.tech';
 
   useEffect(() => {
     const abortController = new AbortController();
