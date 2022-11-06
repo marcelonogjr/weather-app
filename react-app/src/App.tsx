85bf79a5bc5b29eca191c67fccb0f35d533447c3
marcelonogjr <97480299+marcelonogjr@users.noreply.github.com>
2022-11-05 22:57:24 -0300

    fixed the navigation behavior of not scrolling up when changing routes

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,21 @@
-import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom';
 
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
 import styles from './App.module.css';
 
 const App: React.FC = () => {
+  const location = useLocation();
+  const pathName = location.pathname;
+  
+  useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  }, [pathName]);
+
   return (
     <>
       <div className={styles['app-component']}>
