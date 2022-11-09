16326274bf940693cb297892194a2d848ba67976
marcelonogjr <97480299+marcelonogjr@users.noreply.github.com>
2022-11-09 11:38:21 -0300

    fixed hidden mobile header nav menu overflowing on landscape mode

--- react-app/src/components/Layout/Header.tsx
+++ react-app/src/components/Layout/Header.tsx
@@ -118,96 +118,98 @@ const Header = () => {
           <GitHubLogo />
           <ThemeButton />
         </div>
-        <nav className={styles['hamburguer-nav_buttons']}>
-          <Link
-            to='/weather'
-            className={`${styles['link-hamb']} ${
-              styles[`${currentPath === 'weather' ? 'link-hamb-selected' : ''}`]
-            }`}
-            onClick={backdropHandler}
-          >
-            Home
-          </Link>
-          <Link
-            to='/about/intro'
-            className={`${styles['link-hamb']} ${
-              styles[`${currentPath === 'about' ? 'link-hamb-selected' : ''}`]
-            }`}
-            onClick={backdropHandler}
-          >
-            About
-          </Link>
-          <Link
-            to='/about/intro'
-            className={`${styles['link-hamb']} ${styles['sub']} ${
-              styles[
-                `${currentSubPath === 'intro' ? 'link-hamb-sub-selected' : ''}`
-              ]
-            } ${currentPath !== 'about' ? styles['not-active'] : ''}`}
-            onClick={backdropHandler}
-          >
-            Intro
-          </Link>
-          <Link
-            to='/about/map-tiles'
-            className={`${styles['link-hamb']} ${styles['sub']} ${
-              styles[
-                `${currentSubPath === 'map-tiles' ? 'link-hamb-sub-selected' : ''}`
-              ]
-            } ${currentPath !== 'about' ? styles['not-active'] : ''}`}
-            onClick={backdropHandler}
-          >
-            Map Coordinates
-          </Link>
-          <Link
-            to='/about/color-transform'
-            className={`${styles['link-hamb']} ${styles['sub']} ${
-              styles[
-                `${
-                  currentSubPath === 'color-transform'
-                    ? 'link-hamb-sub-selected'
-                    : ''
-                }`
-              ]
-            } ${currentPath !== 'about' ? styles['not-active'] : ''}`}
-            onClick={backdropHandler}
-          >
-            Color Transformation
-          </Link>
-          <Link
-            to='/about/dynamic-graph'
-            className={`${styles['link-hamb']} ${styles['sub']} ${
-              styles[
-                `${
-                  currentSubPath === 'dynamic-graph' ? 'link-hamb-sub-selected' : ''
-                }`
-              ]
-            } ${currentPath !== 'about' ? styles['not-active'] : ''}`}
-            onClick={backdropHandler}
-          >
-            Dynamic Graph Color
-          </Link>
-          <Link
-            to='/about/conclusion'
-            className={`${styles['link-hamb']} ${styles['sub']} ${
-              styles[
-                `${currentSubPath === 'conclusion' ? 'link-hamb-sub-selected' : ''}`
-              ]
-            } ${currentPath !== 'about' ? styles['not-active'] : ''}`}
-            onClick={backdropHandler}
-          >
-            Conclusion
-          </Link>
-          <Link
-            to='/help'
-            className={`${styles['link-hamb']} ${
-              styles[`${currentPath === 'help' ? 'link-hamb-selected' : ''}`]
-            }`}
-            onClick={backdropHandler}
-          >
-            Help
-          </Link>
-        </nav>
+        <div className={styles['hamburguer-nav_container']}>
+          <nav className={styles['hamburguer-nav_buttons']}>
+            <Link
+              to='/weather'
+              className={`${styles['link-hamb']} ${
+                styles[`${currentPath === 'weather' ? 'link-hamb-selected' : ''}`]
+              }`}
+              onClick={backdropHandler}
+            >
+              Home
+            </Link>
+            <Link
+              to='/about/intro'
+              className={`${styles['link-hamb']} ${
+                styles[`${currentPath === 'about' ? 'link-hamb-selected' : ''}`]
+              }`}
+              onClick={backdropHandler}
+            >
+              About
+            </Link>
+            <Link
+              to='/about/intro'
+              className={`${styles['link-hamb']} ${styles['sub']} ${
+                styles[
+                  `${currentSubPath === 'intro' ? 'link-hamb-sub-selected' : ''}`
+                ]
+              } ${currentPath !== 'about' ? styles['not-active'] : ''}`}
+              onClick={backdropHandler}
+            >
+              Intro
+            </Link>
+            <Link
+              to='/about/map-tiles'
+              className={`${styles['link-hamb']} ${styles['sub']} ${
+                styles[
+                  `${currentSubPath === 'map-tiles' ? 'link-hamb-sub-selected' : ''}`
+                ]
+              } ${currentPath !== 'about' ? styles['not-active'] : ''}`}
+              onClick={backdropHandler}
+            >
+              Map Coordinates
+            </Link>
+            <Link
+              to='/about/color-transform'
+              className={`${styles['link-hamb']} ${styles['sub']} ${
+                styles[
+                  `${
+                    currentSubPath === 'color-transform'
+                      ? 'link-hamb-sub-selected'
+                      : ''
+                  }`
+                ]
+              } ${currentPath !== 'about' ? styles['not-active'] : ''}`}
+              onClick={backdropHandler}
+            >
+              Color Transformation
+            </Link>
+            <Link
+              to='/about/dynamic-graph'
+              className={`${styles['link-hamb']} ${styles['sub']} ${
+                styles[
+                  `${
+                    currentSubPath === 'dynamic-graph' ? 'link-hamb-sub-selected' : ''
+                  }`
+                ]
+              } ${currentPath !== 'about' ? styles['not-active'] : ''}`}
+              onClick={backdropHandler}
+            >
+              Dynamic Graph Color
+            </Link>
+            <Link
+              to='/about/conclusion'
+              className={`${styles['link-hamb']} ${styles['sub']} ${
+                styles[
+                  `${currentSubPath === 'conclusion' ? 'link-hamb-sub-selected' : ''}`
+                ]
+              } ${currentPath !== 'about' ? styles['not-active'] : ''}`}
+              onClick={backdropHandler}
+            >
+              Conclusion
+            </Link>
+            <Link
+              to='/help'
+              className={`${styles['link-hamb']} ${
+                styles[`${currentPath === 'help' ? 'link-hamb-selected' : ''}`]
+              }`}
+              onClick={backdropHandler}
+            >
+              Help
+            </Link>
+          </nav>
+        </div>
       </div>
     </>
   );
